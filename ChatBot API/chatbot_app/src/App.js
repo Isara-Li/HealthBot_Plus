cb9a6437a44327b986053dac9b1cca218ae1d48c
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-08-18 00:42:44 +0530

    Add the src folder

---
+++ ChatBot API/chatbot_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AudioRecorder from './AudioRecorder';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Medical Diagnostic Assistant</h1>
+                <AudioRecorder />
+            </header>
+        </div>
+    );
+}
+
+export default App;
