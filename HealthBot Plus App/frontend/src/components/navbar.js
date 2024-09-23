e33c93a8fefc3089501fe70e47440acd3ba8d3b4
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-09-23 21:41:50 +0530

    Update the navbar to direct to homepage

--- HealthBot Plus App/frontend/src/components/navbar.js
+++ HealthBot Plus App/frontend/src/components/navbar.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import NavbarButton from "./navbar_button";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 function Navbar() {
   const navigate = useNavigate();
-  const {currentUser} = useSelector(state => state.user) // get the user from the redux store
-
+  const { currentUser } = useSelector((state) => state.user); // get the user from the redux store
 
   const links_1 = [
     { label: "About", href: "#" },
     { label: "Medical Awards", href: "#" },
-   
   ];
 
   const links = [
-    { label: "Malanoma Detection", href: "#" },
+    { label: "Melanoma Detection", href: "#" },
     { label: "Skin Diseases", href: "#" },
     { label: "ChatBot", href: "#" },
   ];
@@ -24,18 +22,28 @@ function Navbar() {
     { label: "Patient Stories", href: "#" },
     { label: "Doctor Stories", href: "#" },
   ];
+
   const handleLoginSignupClick = () => {
     navigate("/login_signup");
   };
+
   const handleProfile = () => {
     navigate(`/patient/${currentUser._id}`);
   };
 
+  const handleLogoClick = () => {
+    navigate("/"); // Redirect to the home page
+  };
+
   return (
     <div className="flex bg-white border border-slate-200 h-18 font-sans z-[999] relative">
-      
       <div className="w-1/4 flex justify-center items-center">
-        <img src={"../images/HealthBot+.PNG"} alt="Logo" className="h-5/6" />
+        <img
+          src={"../images/HealthBot+.PNG"}
+          alt="Logo"
+          className="h-5/6 cursor-pointer"
+          onClick={handleLogoClick} // Add onClick to redirect to home page
+        />
       </div>
       <div className="w-3/4 flex justify-center">
         <div className="w-4/5 h-full flex">
@@ -49,19 +57,25 @@ function Navbar() {
             <NavbarButton label="Stories" links={links_2} />
           </div>
           {currentUser ? (
-          <div className='flex-1 flex justify-center items-center hover:bg-slate-200 rounded-xl cursor-pointer' onClick={handleProfile}>
-            <img className='rounded-full h-7 w-7 object-cover mr-4' src={currentUser.profile} alt='Profile Pic'></img>
-            {currentUser.name}
-          </div>
-        ):(
-          <div
-          className="flex-1 hover:bg-slate-200 transform transition duration-400 flex justify-center items-center font-semibold cursor-pointer rounded-xl"
-          onClick={handleLoginSignupClick}
-        >
-          Login / SignUp
-        </div>
-        )}
-         
+            <div
+              className="flex-1 flex justify-center items-center hover:bg-slate-200 rounded-xl cursor-pointer"
+              onClick={handleProfile}
+            >
+              <img
+                className="rounded-full h-7 w-7 object-cover mr-4"
+                src={currentUser.profile}
+                alt="Profile Pic"
+              ></img>
+              {currentUser.name}
+            </div>
+          ) : (
+            <div
+              className="flex-1 hover:bg-slate-200 transform transition duration-400 flex justify-center items-center font-semibold cursor-pointer rounded-xl"
+              onClick={handleLoginSignupClick}
+            >
+              Login / SignUp
+            </div>
+          )}
         </div>
       </div>
     </div>
