00db9dc65b118a18107759e01e2197c6ded36ad5
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-09-23 23:38:51 +0530

    Langchain integration for chatbot

--- HealthBot Plus App/backend/chatbot.py
+++ HealthBot Plus App/backend/chatbot.py
@@ -3,6 +3,13 @@ import requests
 import json
 from flask import jsonify
 import os
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+import streamlit as st
+
 
 token_hugging_face = "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"
 
@@ -11,6 +18,14 @@ API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/openai/whispe
 API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
 
 openai.api_key = "sk-ZjPyGb_zTnLPx-1ko4FGy5UGDQnEBxyRoNkrqklKe5T3BlbkFJWEjSwB0_jZZ4euSpAnyY9vksNqAPc5_Zg1iKnA3cwA"
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=r'C:\Users\Isara Liyanage\Documents\GitHub\HealthBot_Plus\HealthBot Plus App\backend\.env')
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
 
 
 
@@ -54,5 +69,26 @@ def diagnose(request):
     print("Diagnosis",diagnosis)
     if diagnosis is None:
         return jsonify({"error": "Failed to predict disease"}), 500
+    ## langchain
+
+    prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+    ## streamlit framework
+
+    st.title('Langchain Demo With OPENAI API')
+    input_text=text
+
+    # openAI LLm 
+    llm=ChatOpenAI(model="gpt-3.5-turbo")
+    output_parser=StrOutputParser()
+    chain=prompt|llm|output_parser
+
+    if input_text:
+        st.write(chain.invoke({'question':input_text}))
 
     return jsonify({"text": text, "diagnosis": diagnosis})
